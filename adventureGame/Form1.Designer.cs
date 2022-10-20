6188ae2a254677eaf22e3ba8ac5ae197b207b7fb
Dominic Winter-Pacheco <domiwint423@ed.amdsb.ca>
2022-10-20 11:25:47 -0400

    Add project files.

---
+++ adventureGame/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace adventureGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.option1Button = new System.Windows.Forms.Button();
+            this.option2Button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.output1Label = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // option1Button
+            // 
+            this.option1Button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.option1Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.option1Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.option1Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1Button.Location = new System.Drawing.Point(64, 370);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(38, 31);
+            this.option1Button.TabIndex = 0;
+            this.option1Button.Text = "1";
+            this.option1Button.UseVisualStyleBackColor = true;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // option2Button
+            // 
+            this.option2Button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.option2Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.option2Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.option2Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2Button.Location = new System.Drawing.Point(64, 407);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(38, 31);
+            this.option2Button.TabIndex = 1;
+            this.option2Button.Text = "2";
+            this.option2Button.UseVisualStyleBackColor = true;
+            this.option2Button.Click += new System.EventHandler(this.option2Button_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(117, 407);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(323, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hide";
+            // 
+            // output1Label
+            // 
+            this.output1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output1Label.ForeColor = System.Drawing.Color.White;
+            this.output1Label.Location = new System.Drawing.Point(117, 370);
+            this.output1Label.Name = "output1Label";
+            this.output1Label.Size = new System.Drawing.Size(323, 31);
+            this.output1Label.TabIndex = 2;
+            this.output1Label.Text = "Look around";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(12, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(428, 86);
+            this.titleLabel.TabIndex = 4;
+            this.titleLabel.Text = "You are stuck in a pizzeria with killer animatronics. Do you Hide or do you Look " +
+    "Around";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::adventureGame.Properties.Resources.Ebn0Kq1UMAAmU1m;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(452, 450);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.output1Label);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option1Button);
+            this.Name = "Form1";
+            this.Text = "adventureGame";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label output1Label;
+        private System.Windows.Forms.Label titleLabel;
+    }
+}
+
---
+++ adventureGame/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace adventureGame
+{
+    public partial class Form1 : Form
+    {
+        int page = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 4;
+
+            }
+            else if (page == 3)
+            {
+                page = 9;
+
+            }
+            else if (page == 4)
+            {
+                page = 6;
+            }
+            else if (page == 5)
+            {
+                page = 11;
+            }
+            else if (page == 6)
+            {
+                page = 8;
+            }
+            else if (page == 7)
+            {
+                page = 99;
+
+            }
+            else if (page == 8)
+            {
+                page = 99;
+
+            }
+            else if (page == 9)
+            {
+                page = 99;
+            }
+            else if (page == 10)
+            {
+                page = 99;
+            }
+            else if (page == 11)
+            {
+                page = 99;
+
+            }
+            else if (page == 12)
+            {
+                page = 13;
+
+            }
+            else if (page == 13)
+            {
+                page = 4;
+
+            }
+            else if (page == 14)
+            {
+                page = 16;
+            }
+            else if (page == 15)
+            {
+                page = 17;
+            }
+            else if (page == 16)
+            {
+                page = 20;
+            }
+            else if (page == 17)
+            {
+                page = 20;
+
+            }
+            else if (page == 18)
+            {
+                page = 99;
+
+            }
+            else if (page == 19)
+            {
+                page = 24;
+            }
+            else if (page == 20)
+            {
+                page = 22;
+            }
+            else if (page == 21)
+            {
+                page = 99;
+
+            }
+            else if (page == 22)
+            {
+                page = 20;
+
+            }
+            else if (page == 23)
+            {
+                page = 25;
+
+            }
+            else if (page == 24)
+            {
+                page = 99;
+            }
+            else if (page == 25)
+            {
+                page = 26;
+            }
+            else if (page == 26)
+            {
+                page = 99;
+            }
+            else if (page == 27)
+            {
+                page = 99;
+
+            }
+            else if (page == 28)
+            {
+                page = 99;
+
+            }
+            else if (page == 99)
+            {
+                page = 1;
+            }
+            
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page == 2)
+            {
+                page = 12;
+
+            }
+            else if (page == 3)
+            {
+                page = 5;
+
+            }
+            else if (page == 4)
+            {
+                page = 14;
+            }
+            else if (page == 5)
+            {
+                page = 7;
+            }
+            else if (page == 6)
+            {
+                page = 28;
+            }
+            else if (page == 7)
+            {
+                page = 99;
+
+            }
+            else if (page == 8)
+            {
+                page = 99;
+
+            }
+            else if (page == 9)
+            {
+                page = 99;
+            }
+            else if (page == 10)
+            {
+                page = 99;
+            }
+            else if (page == 11)
+            {
+                page = 99;
+
+            }
+            else if (page == 12)
+            {
+                page = 21;
+
+            }
+            else if (page == 13)
+            {
+                page = 4;
+
+            }
+            else if (page == 14)
+            {
+                page = 15;
+            }
+            else if (page == 15)
+            {
+                page = 18;
+            }
+            else if (page == 16)
+            {
+                page = 19;
+            }
+            else if (page == 17)
+            {
+                page = 20;
+
+            }
+            else if (page == 18)
+            {
+                page = 99;
+
+            }
+            else if (page == 19)
+            {
+                page = 27;
+            }
+            else if (page == 20)
+            {
+                page = 23;
+            }
+            else if (page == 21)
+            {
+                page = 99;
+
+            }
+            else if (page == 22)
+            {
+                page = 20;
+
+            }
+            else if (page == 23)
+            {
+                page = 19;
+
+            }
+            else if (page == 24)
+            {
+                page = 99;
+            }
+            else if (page == 25)
+            {
+                page = 26;
+            }
+            else if (page == 26)
+            {
+                page = 99;
+            }
+            else if (page == 27)
+            {
+                page = 99;
+
+            }
+            else if (page == 28)
+            {
+                page = 99;
+
+            }
+            else if (page == 99)
+            {
+                page = 1;
+            }
+        }
+    }
+}
